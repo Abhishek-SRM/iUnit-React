285b85e52c6e790e93f461358791f940cd9a1bdb
Abhishek Singh <91311402+Abhishek-SRM@users.noreply.github.com>
2023-08-08 10:59:23 +0530

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,12 @@ import { ExtensionContext, ExtensionMode, Uri, Webview } from 'vscode';
 import { MessageHandlerData } from '@estruyf/vscode';
 this is a Message from Abhishek Singh 
 ansd this is another message 
+edqwe
+dqwd
+qwd
+qwdqw
+dqw
+d
 export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('vscode-react-webview-starter.openWebview', () => {
